a0cecf96de6359adbb0c4b04e1cbc519a2d67322
Julian Rios <julianelsensei@gmail.com>
2023-08-08 15:45:09 -0300

    Adjunte clase

---
+++ Git2/src/git2/ClaseJulian.java
@@ -0,0 +1,46 @@
+
+package git2;
+
+/**
+ 
+ * @author Julian Development
+ */
+public class ClaseJulian {
+    
+    String nombre;
+    int edad;
+    String localidad;
+
+    public ClaseJulian(String nombre, int edad, String localidad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.localidad = localidad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+    
+    
+
+}
